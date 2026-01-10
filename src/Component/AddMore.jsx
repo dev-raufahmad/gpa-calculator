85252456a7cd09cbe310e22fc6294e9b3fc4a0b0
Rauf Ahmad Khan <raufahmadkhan9@gmail.com>
2026-01-10 23:16:56 +0500

    Components Folder Added

---
+++ src/Component/AddMore.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const AddMore = () => {
+    return (
+        <div>
+            <input type="number" placeholder="Enter total marks " />
+            <input type="number" placeholder="Enter obtained marks " />
+        </div>
+    );
+};
