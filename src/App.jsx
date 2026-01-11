d978272f8848b46d0b2e1c524dcc2a9e2f45c82f
Rauf Ahmad Khan <raufahmadkhan9@gmail.com>
2026-01-11 22:11:08 +0500

    Update to add credit hours,subjectName

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from "react"
 import AddMore from "./Component/AddMore"
 import { MyContext } from "./StateManagement/MyContext";
+import { Lab } from "./Component/Lab";
 
 const clicking = (quizMarks, setQuizMarks,isQuiz) => {
   if (quizMarks.length == 4) {
@@ -14,15 +15,14 @@ const clicking = (quizMarks, setQuizMarks,isQuiz) => {
   console.log(quizMarks);
 
 }
+
+
+// Here is the start of the main APP function
 function App() {
 
 
-  const updateQuiz = (id, obtainedMarks, totalMarks) => {
-    // if(obtainedMarks < totalMarks){
-    //   console.log("Invalid Marks");
-    //   return;
-    // }
 
+  const updateQuiz = (id, obtainedMarks, totalMarks) => {
     setQuizMarks(quizMarks.map((e) => {
       return e.id === id ? { ...e, obtainedMarks: obtainedMarks, totalMarks: totalMarks } : e;
     }))
@@ -39,6 +39,11 @@ function App() {
   const [midTermTotal, setMidTermTotal] = useState();
   const [finalterm, setFinalterm] = useState();
   const [finalTermTotal, setFinalTermTotal] = useState();
+
+  const [title , setTitle] = useState("");
+  const [creditHours , setCreditHours] = useState();
+  const [hasLab, setHasLab] = useState(false);
+
 
   const marksCalculator = (input, number) => {
     let totalObtained = Number(0);
@@ -72,7 +77,24 @@ function App() {
 
   return (
     <>
-      <h1>GPA Calculator</h1>
+      <div>
+        <h1>{title} {creditHours}</h1>
+      </div>
+      <div>
+        {/* This div for the labeling of the Subject */}
+        <div>
+            <label htmlFor="labeling">Subject Name</label>
+            <input id="labeling" type="text" value={title} onChange={(e) =>setTitle(e.target.value)} placeholder="Name of Subject here"/>
+        </div>
+        {/* This div is for the Credit hours of the course */}
+        <div>
+          <label htmlFor="creditHours">Credit Hours</label>
+          <input type="number" value={creditHours} onChange={(e) => setCreditHours(e.target.value)} id="creditHours" />
+        </div>
+        {/* This div is for the Has-Lab property */}
+        <label htmlFor="hasLab">Has Lab</label>
+        <input type="checkbox" value={hasLab} onChange={() => setHasLab(!hasLab)} />
+      </div>
       {/* Here is the start of the quizes Marks */}
       <div>
         <h1>Quiz Marks</h1>
@@ -114,7 +136,10 @@ function App() {
         <input type="number" value={finalTermTotal} onChange={(e) => setFinalTermTotal(e.target.value)} placeholder="Total Marks" />
         <input type="number" value={finalterm} onChange={(e) => setFinalterm(e.target.value)} placeholder="Obtained Marks" />
       </div>
-
+      {/* Thi section is for the Lab */}
+      <div>
+        {hasLab && <Lab/>}
+      </div>
       <div>
         Your Marks till now is : {calculating()}
       </div>
---
+++ src/Component/Lab.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export const Lab =() => {
+    return(
+        <h1>Here is the Lab component</h1>
+    )
+} 
